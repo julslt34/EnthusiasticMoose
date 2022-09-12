f271cc1b5fe2a18a069afc5f4e4de999d4331664
Julie Adams <julslt34@gmail.com>
2022-09-12 14:28:05 -0400

    Initial save

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnthusiasticMoose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Enthusistic Moose Simulator!");
+        }
+    }
+}
